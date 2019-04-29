2bd4bef6083fdaabfd11dee8763b42d419bd4c9d
honeypigs <1002213567@qq.com>
2019-04-29 14:44:14 +0800

    change

--- js/project.js
+++ js/project.js
@@ -1,6 +1,7 @@
 $.get("http://api.tronhoo3d.com/products",function (res) {
 	var data = res.data;
 	var diyData = data[1], eduData = data[2], indData = data[3]; 
+    // console.log(diyData,eduData,indData);
 	var vmdiy = new Vue({
                 el: "#diy",
                 data: diyData,
@@ -12,10 +13,20 @@ $.get("http://api.tronhoo3d.com/products",function (res) {
             });
 	var vmedu = new Vue({
                 el: "#edu",
-                data: eduData
+                data: eduData,
+                methods: {
+                    detail: function(event){
+                        window.location = "detail.html#" + event.target.id;
+                    }
+                }
             });
 	var vmind = new Vue({
                 el: "#ind",
-                data: indData
+                data: indData,
+                methods: {
+                    detail: function(event){
+                        window.location = "detail.html#" + event.target.id;
+                    }
+                }
             });
 })
