46fce370909a617ee80d734dbd8316778661acfe
honeypigs <1002213567@qq.com>
2019-10-29 15:19:46 +0800

    final pakage

--- js/services.js
+++ js/services.js
@@ -1,36 +1,35 @@
 function formatDate (date, fmt) {
-    if (/(y+)/.test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-    }
-    let o = {
-        'M+': date.getMonth() + 1,
-        'd+': date.getDate(),
-        'h+': date.getHours(),
-        'm+': date.getMinutes(),
-        's+': date.getSeconds()
-    };
-    for (let k in o) {
-        if (new RegExp(`(${k})`).test(fmt)) {
-            let str = o[k] + '';
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
-        }
-    }
-    return fmt;
+	if (/(y+)/.test(fmt)) {
+		fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+	}
+	let o = {
+		'M+': date.getMonth() + 1,
+		'd+': date.getDate(),
+		'h+': date.getHours(),
+		'm+': date.getMinutes(),
+		's+': date.getSeconds()
+	};
+	for (let k in o) {
+		if (new RegExp(`(${k})`).test(fmt)) {
+			let str = o[k] + '';
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+		}
+	}
+	return fmt;
 };
 
 function padLeftZero (str) {
-    return ('00' + str).substr(str.length);
+	return ('00' + str).substr(str.length);
 };
 
 $.get("http://api.tronhoo3d.com/models",function (res) {
-    var mData = res;
-    console.log(mData);
-    var model = new Vue({
-        el: "#models",
-        data: mData,
-//          mounted: function() {
-//              
-//          
+	var mData = res;
+	var model = new Vue({
+		el: "#models",
+		data: mData,
+//        	mounted: function() {
+//        		
+//        	
 //            var filelist = [];
 //            var id = [];
 //            for(var i = 0;i < mData.data.length;i++) {
@@ -42,51 +41,51 @@ $.get("http://api.tronhoo3d.com/models",function (res) {
 //            }
 //            for(var i = 0; i < filelist.length;i++) {
 //                var everRender = evercad.render3d(id[i], filelist[i],{
-//              background: '#f0f0f0',     // 背景色
-//               showToolbar: false,        // 是否显示 toolbar
-//               logo: {
-//                 right: '40px',           // 距右边 40px
-//                 bottom: '100px',         // 距底边 100px
-//                 width: '40px',           // logo 宽度，小于 40px 为 40px
-//                 src: 'https://img.com/logo.jpg'
-//               }
-//          })
+//				background: '#f0f0f0',     // 背景色
+//			     showToolbar: false,        // 是否显示 toolbar
+//			     logo: {
+//			       right: '40px',           // 距右边 40px
+//			       bottom: '100px',         // 距底边 100px
+//			       width: '40px',           // logo 宽度，小于 40px 为 40px
+//			       src: 'https://img.com/logo.jpg'
+//			     }
+//	     	})
 //            }
-//          
+//			
 //        }
-    })    
+	})    
 })
 
 
 $.get("http://api.tronhoo3d.com/software",function (res) {
-    var sData = res;
-    var soft = new Vue({
-        el: "#software",
-        data: sData
-    })
+	var sData = res;
+	var soft = new Vue({
+		el: "#software",
+		data: sData
+	})
 })
 
 $.get("http://api.tronhoo3d.com/files",function (res) {
-    var fData = res;
-    var file = new Vue({
-        el: "#files",
-        data: fData
-    })
+	var fData = res;
+	var file = new Vue({
+		el: "#files",
+		data: fData
+	})
 })
 
 $.get("http://api.tronhoo3d.com/questions",function (res) {
-    var qData = res;
-    var ques = new Vue({
-        el: "#ques",
-        data: qData
-    })
+	var qData = res;
+	var ques = new Vue({
+		el: "#ques",
+		data: qData
+	})
 })
 $.get("http://api.tronhoo3d.com/information",function (res) {
-    var nData = res;
-    var news = new Vue({
-        el: "#news",
-        data: nData,
-        methods: {
+	var nData = res;
+	var news = new Vue({
+		el: "#news",
+		data: nData,
+		methods: {
             jump: function() {
                if(window.location.hash != ""){
                     window.location.href = window.location.hash;
@@ -96,38 +95,39 @@ $.get("http://api.tronhoo3d.com/information",function (res) {
         mounted: function(){
             this.jump();
         }
-    })
+	})
 })
 
 
 function show(event) {
-    if(event.target.nextElementSibling.className == "content") {
-        event.target.nextElementSibling.className = "contents";
-    }else {
-        event.target.nextElementSibling.className = "content";
-    }
+	if(event.target.nextElementSibling.className == "content") {
+		event.target.nextElementSibling.className = "contents";
+	}else {
+		event.target.nextElementSibling.className = "content";
+	}
 }
 
 function showmodel(event) {
-    console.log(event);
-    var e = event.target;
-    var path = e.nextElementSibling.href;
+	var e = event.target;
+	var path = e.nextElementSibling.href;
      var name = e.id + e.type;
      var link = [{ path: path, name: name}];
-     console.log(link);
      var id = "#" + e.nextElementSibling.nextElementSibling.id;
-     console.log(id);
+     var height = e.parentNode.clientHeight;
+     var margin = e.clientHeight;
      var everRender = evercad.render3d(id, link,{
-        background: '#f0f0f0',     // 背景色
-         showToolbar: false,        // 是否显示 toolbar
-         logo: {
-           right: '40px',           // 距右边 40px
-           bottom: '100px',         // 距底边 100px
-           width: '40px',           // logo 宽度，小于 40px 为 40px
-           src: 'https://img.com/logo.jpg'
-         }
+		background: '#f0f0f0',     // 背景色
+	     showToolbar: false,        // 是否显示 toolbar
+	     logo: {
+	       right: '0px',           // 距右边 40px
+	       bottom: '0px',         // 距底边 100px
+	       width: '40px',// logo 宽度，小于 40px 为 40px
+	       height: '40px'
+	     }
      });
      e.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "none";
+     e.parentNode.style.height = height + "px";
+     e.nextElementSibling.nextElementSibling.style.marginTop = margin + "px";
 }
 
 var list = document.querySelector("#list");
@@ -156,14 +156,14 @@ list.onclick = function(e) {
         document.querySelector("#files").hidden = true;
         document.querySelector("#ques").hidden = false;
         document.querySelector("#news").hidden = true;
-    } else if(e.target.hash=="#newsl"){
+    } else if(e.target.hash == "#newsl"){
         document.querySelector("#models").hidden = true;
         document.querySelector("#software").hidden = true;
         document.querySelector("#files").hidden = true;
         document.querySelector("#ques").hidden = true;
         document.querySelector("#news").hidden = false;
     } else{
-        document.querySelector("#models").hidden = false;
+    	   document.querySelector("#models").hidden = false;
         document.querySelector("#software").hidden = false;
         document.querySelector("#files").hidden = false;
         document.querySelector("#ques").hidden = false;
@@ -181,3 +181,6 @@ function changefix() {
         document.querySelector("#list").style.position = "absolute";
     }
 }
+
+
+
